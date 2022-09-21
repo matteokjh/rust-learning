79cc0f0a70bf9a82886cc77ac12322eedf718ac3
matteokjh <429797371@qq.com>
2022-09-22 02:36:11 +0800

    feat: cargo

---
+++ cargo-demo/target/debug/cargo-demo.d
@@ -0,0 +1,1 @@
+/Users/matteokjh/Documents/projects/rust-learning/cargo-demo/target/debug/cargo-demo: /Users/matteokjh/Documents/projects/rust-learning/cargo-demo/src/main.rs
---
+++ cargo-demo/target/debug/deps/cargo_demo-3af05b669f4973d2.d
@@ -0,0 +1,5 @@
+/Users/matteokjh/Documents/projects/rust-learning/cargo-demo/target/debug/deps/cargo_demo-3af05b669f4973d2.rmeta: src/main.rs
+
+/Users/matteokjh/Documents/projects/rust-learning/cargo-demo/target/debug/deps/cargo_demo-3af05b669f4973d2.d: src/main.rs
+
+src/main.rs:
